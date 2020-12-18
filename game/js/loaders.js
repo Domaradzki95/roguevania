fde018dfe25e6ba7826cbd493ce2696f42a986d7
Agee <gamingprodukcje@gmail.com>
2020-12-19 00:54:11 +0100

    merging tiles from different tilesets, crude ver

--- game/js/loaders.js
+++ game/js/loaders.js
@@ -1,7 +1,12 @@
-export function loadMap() {
-    return fetch('../assets/miniTileset/teest.json')
+ export function loadMap() {
+    return fetch('../assets/miniTileset/teest2.json')
     .then(r=> r.json())    
-};
+}; 
+
+/* export function loadMap() {
+    return fetch('../assets/ORGIN/Demo1.json')
+    .then(r=> r.json())    
+}; */
 
 export function loadImage(url) {
     return new Promise(resolve=>{
--- game/js/main.js
+++ game/js/main.js
@@ -1,39 +1,53 @@
 import {loadMap, loadImage} from "./loaders.js";
+import TileSet from "./TileSet.js";
 const canvas = document.getElementById('screen');
 const ctx = canvas.getContext('2d');
 
 ctx.fillRect(100,100,100,100);
 
 
-class TileSet {
-    constructor(gid, columns,tilecount, tileSize) {
-        this.gid = gid;
-        this.columns = columns;
-        this.tilecount = tilecount;
-        this.tileSize = tileSize;
-        this.tiles = [];
+
+
+class createTiles{
+    constructor(img,width,height) {
+        this.img = img;
+        this.tiles = new Map();
+        this.width = width;
+        this.height = height;
     }
-    setTiles() {
-        for(let i = 0; i< this.tilecount; i++) {
-            this.tiles.push({id: i+1, x:null, y:null});
-        }
-    }
-    setTilesPositions() {
-        this.setTiles();
-        this.tiles.forEach((el,index)=> { 
-            if(index>=this.columns) {
-                el.y = Math.floor(index/this.columns);
-                el.x =  index - this.columns * el.y;
-            } else {
-                el.y = 0;
-                el.x = index;
-            }
-        el.x *= this.tileSize;
-        el.y *= this.tileSize;
+    defineTile(id,x,y){
+        const buffer = document.createElement('canvas');
+        buffer.width = this.width;
+        buffer.height = this.height;
+        buffer.getContext('2d')
+              .drawImage(this.img,
+                         x,
+                         y, 
+                         this.width,
+                         this.height,
+                         0,
+                         0,
+                         buffer.width,
+                         buffer.height);
+        this.tiles.set(id,buffer);
         
-        })
+    }
+    drawTile(id,ctx,x,y) {
+        
+        const tile = this.merged.get(id);
+        ctx.drawImage(tile,x,y);
+    };
+
+
+};
+
+class combineAllTiles extends createTiles{
+    constructor(merged){
+        super();
+        this.merged = merged;
     }
 }
+
 
 class setMap {
     constructor(mapW, mapH, layers) {
@@ -51,16 +65,7 @@ class setMap {
             num+= this.mapW;
             matrix.push(row);
        };
-/*        matrix.forEach((row, index)=>{
-           let x;
-           let y = index;
-           row.forEach((el,rowIndex) =>{
-                if(el) {
-                    x= rowIndex;
-                }
-           });
-           console.log(x,y);
-       });  */
+
        return matrix;     
     }
     setWorldLayerMatrixes() {
@@ -70,26 +75,108 @@ class setMap {
             this.worldMatrix.set(layer.name, lastMatrix);
         });
     };
+};
+
+ class renderMap {
+    constructor(mapData, tiles) {
+        this.mapData = mapData;
+        this.tiles = tiles;        
+    }
+
+
+    drawMap(tilesCreator) {
+        this.mapData.worldMatrix.forEach((matrix,key)=>{
+           console.log(tilesCreator);
+            matrix.forEach((row, index)=>{
+                let x;
+                let y = index;
+                row.forEach((el,rowIndex) =>{
+                     if(el) {
+                         x= rowIndex;
+                         console.log(el, x,y, key);
+                       tilesCreator.drawTile(el,ctx,x*16,y*16);
+                        
+                     }
+                 
+                });
+                
+            }); 
+        })
+
+    }
 }
 
 
 let level1 = loadMap();
-//What I have, layers matrixes 
+
 level1
 .then(map => {
+    
+   let tileSetsStorage = [];
+   map.tilesets.forEach(el=> {
+    tileSetsStorage.push(new TileSet(el.firstgid, el.columns, 
+                                     el.tilecount, el.tilewidth));
+   })
+  
 
-    const firstTileset=  map.tilesets[0];
-    let {columns,tilecount, tilewidth} = firstTileset;
-    const miniTileset = new TileSet(firstTileset.firstgid, columns,tilecount, tilewidth);
-    miniTileset.setTilesPositions();
-    //console.log(miniTileset);
-    console.log(map); 
+ tileSetsStorage[0].setTilesPositions();
+ tileSetsStorage[1].setTilesPositions();
+ console.log(tileSetsStorage);
     const mapMatrix = new setMap(map.width, map.height, map.layers);
-    console.log(mapMatrix);
+    
     mapMatrix.setWorldLayerMatrixes();
-    loadImage('../assets/miniTileset/miniTileset.png')
-    .then(image=>{
-        ctx.drawImage(image,0,0);
+    const world = new renderMap(mapMatrix);
+
+     Promise.all([
+        loadImage('../assets/miniTileset/miniTileset.png'),
+        loadImage('../assets/miniTileset/Taiga.png')
+     ])
+      
+  //loadImage('../assets/ORGIN/elo.png')
+    .then((values)=>{
+        let tileRenderers = [];
+        values.forEach(img=> {
+            tileRenderers.push(new createTiles(img,16,16))
+        })
+        
+       
+        
+        tileSetsStorage.forEach((el,index)=>{
+            el.tiles.forEach((tile) => {
+                tileRenderers[index].defineTile(tile.id,tile.x,tile.y);
+
+             })
+        })
+
+        
+      //  console.log(tileRenderers[1]);
+         
+         //world.drawMap(tileRenderers[0]);
+         let merged = new Map([...tileRenderers[0].tiles, ...tileRenderers[1].tiles]);
+         let allTiles = new combineAllTiles(merged);
+         
+        // console.log(merged);
+         world.drawMap(allTiles);
+
+                       // ctx.drawImage(image,0,0);
     })
     
-})
+});
+
+/*      loadImage('../assets/miniTileset/miniTileset.png')
+  //loadImage('../assets/ORGIN/elo.png')
+    .then(image=>{
+
+
+         const tiles = new createTiles(image,16,16);
+
+         miniTileset.tiles.forEach((tile) => {
+            tiles.defineTile(tile.id,tile.x,tile.y);
+         })
+        
+         const world = new renderMap(mapMatrix);
+         
+         world.drawMap(tiles);
+
+                       // ctx.drawImage(image,0,0);
+    }) */
